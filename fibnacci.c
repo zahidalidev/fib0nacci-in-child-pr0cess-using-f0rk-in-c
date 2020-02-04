a2e57e661d7a4c9120a0d54a9064ad3af893c954
Zahid Ali <46484008+zahidalidev@users.noreply.github.com>
2020-02-04 11:52:45 +0500

    Update fibnacci.c

--- fibnacci.c
+++ fibnacci.c
@@ -10,15 +10,14 @@ int main()
   scanf("%d",&n);
   if (n<=0) 
     {
-      printf("moron!\n");
+      printf("invalid \n");
       return 1;
     }
   pid_t pid = fork();
+ 
   if (pid == 0)
     {
-      /*child process*/
-      /*here we print out the fibonacci numbers, which i would do like this*/
-      printf("fibonacci-numbers up to %d:\n",n);
+      printf("printing fibonacci-numbers up to %d:\n",n);
       int *fib_array = (int*)malloc(sizeof(int)*n);
       int i;
       for (i=0;i<n;i++)
@@ -29,7 +28,7 @@ int main()
         fib_array[i] = fib_array[i-1]+fib_array[i-2];
       printf("%d\n",fib_array[i]);
     }
-      free(fib_array); /*not really neccessary, but anyway*/
+      free(fib_array);
       exit(0);
     }
   if (pid < 0)
